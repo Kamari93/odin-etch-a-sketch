7f72037bc380c442b6459c29b5ee77810bf17cde
Kamari <kmoore971@gmail.com>
2023-07-12 08:45:06 -0500

    Update files for range input

--- script.js
+++ script.js
@@ -1,26 +1,32 @@
+console.log("Keep Going 🍊");
+
+// DOM Manipulation
 const sketchPad = document.querySelector(".sketch-pad");
-let row = 16;
-let column = 16;
+const slider = document.querySelector("input");
+const dimensions = document.querySelector("label");
+
+// set the sketch-pad to display grid
 sketchPad.style.display = "grid";
-sketchPad.style.gridTemplateRows = `repeat(${row}, 1fr)`;
-sketchPad.style.gridTemplateColumns = `repeat(${column}, 1fr)`;
 
-for (let i = 0; i < row * column; i++) {
-    box = document.createElement("div");
-    box.classList.add("box");
-    box.style.outline = "solid grey";
-    box.textContent = `${i}`;
-    box.style.textAlign = "center";
-    sketchPad.appendChild(box);
+// test slider value
+console.log(slider.value);
+dimensions.textContent = `${slider.value} x ${slider.value}`;
+
+// change number of squares in sketchpad
+getsquares(10);
+
+
+// function to get the total squares of sketch pad
+function getsquares(rows = 16) {
+    let columns = rows;
+    sketchPad.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
+    sketchPad.style.gridTemplateColumns = `repeat(${columns}, 1fr)`;
+    for (let i = 0; i < rows * columns; i++) {
+        box = document.createElement("div");
+        box.classList.add("box");
+        box.style.outline = "solid grey";
+        box.textContent = `${i}`;
+        box.style.textAlign = "center";
+        sketchPad.appendChild(box);
+    }
 }
-// let box;
-
-// for (let i = 0; i <= 682; i++) {
-//     box = document.createElement("div");
-//     box.classList.add("box");
-//     sketchPad.appendChild(box);
-// }
-
-// function getsquares(rows, columns) {
-
-// }
