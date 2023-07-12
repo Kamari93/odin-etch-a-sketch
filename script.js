b0578aa83117ccd6ea4a3c2234f7a3c121654cf3
Kamari <kmoore971@gmail.com>
2023-07-12 10:33:28 -0500

    Remove grid item texts to dynamic fit divs

--- script.js
+++ script.js
@@ -4,17 +4,24 @@ console.log("Keep Going 🍊");
 const sketchPad = document.querySelector(".sketch-pad");
 const slider = document.querySelector("input");
 const dimensions = document.querySelector("label");
+dimensions.textContent = `${slider.value} x ${slider.value} `;
 
 // set the sketch-pad to display grid
 sketchPad.style.display = "grid";
 
-// test slider value
-console.log(slider.value);
-dimensions.textContent = `${slider.value} x ${slider.value}`;
+// Listen for slider value changes
+slider.addEventListener("input", getSlider);
 
-// change number of squares in sketchpad
-getsquares(10);
+// set the initial number of boxes on start
+getsquares(slider.value);
 
+// update slider value on user input
+function getSlider() {
+    console.log(slider.value);
+    dimensions.textContent = `${slider.value} x ${slider.value}`;
+    // change number of squares in sketchpad
+    getsquares(slider.value);
+}
 
 // function to get the total squares of sketch pad
 function getsquares(rows = 16) {
@@ -25,7 +32,8 @@ function getsquares(rows = 16) {
         box = document.createElement("div");
         box.classList.add("box");
         box.style.outline = "solid grey";
-        box.textContent = `${i}`;
+        // box.textContent = `${i}`;
+        box.setAttribute("id", `${i}`);
         box.style.textAlign = "center";
         sketchPad.appendChild(box);
     }
